78fc3f564dd7db1e235f7902d91a8de3b45509db
Cristian Andres Rojas Vargas <crisanrova@gmail.com>
2025-02-24 09:12:29 -0500

    Delete segimiento 3 directory

--- segimiento 3/ejercicio 2.js
+++
@@ -1,8 +0,0 @@
-let Numero = prompt("Ingrese un numero: ")
-
-if (Numero % 2 == 0) {
-document.write(`El numero ${Numero} es par`)
-}
-else {
-document.write(`El numero ${Numero} es impar`)
-}
--- segimiento 3/ejercicio 6.js
+++
@@ -1,7 +0,0 @@
-let Numero1 = prompt("Ingrese un número")
-let Numero2 = prompt("Ingrese otro número")
-let Numero3 = prompt("Ingrese otro número")
-
-let Mayor = Math.max(Numero1, Numero2, Numero3)
-
-document.write(`El número mayor es ${Mayor}`)
